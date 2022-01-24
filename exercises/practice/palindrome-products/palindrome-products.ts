d8d22acad48b2254ac2bcd6e2e5bba9056f30cd1
Tim Hor <timothy.hor@outlook.com>
2022-01-24 12:15:27 +1100

    Start palindrome-products

--- exercises/practice/palindrome-products/palindrome-products.ts
+++ exercises/practice/palindrome-products/palindrome-products.ts
@@ -1,8 +1,52 @@
 interface Input {
-  maxFactor: number
-  minFactor?: number
+  maxFactor: number;
+  minFactor: number;
 }
 
-export function generate(params: Input): unknown {
-  throw new Error('Remove this statement and implement this function')
+type Factors = [number, number][];
+
+type Palindrome = {
+  value: number;
+  factors: Factors;
+};
+
+type Output = {
+  smallest: Palindrome;
+  largest: Palindrome;
+};
+
+const isPalindrome = (candidate: number) =>
+  candidate.toString().split('').reverse().join('') === candidate.toString();
+
+export function generate(params: Input): Output {
+  const { minFactor, maxFactor } = params;
+  const allNumbers = [...Array(maxFactor + 1).keys()].filter(
+    (num) => num >= minFactor,
+  );
+  let palindromeProducts = new Set<number>();
+  let palindromeFactors: { [index: number]: Factors } = {};
+  for (let i = 0; i < allNumbers.length; i++) {
+    for (let j = 0; j < allNumbers.length; j++) {
+      const candidate = allNumbers[i] * allNumbers[j];
+      if (isPalindrome(candidate)) {
+        palindromeProducts.add(candidate);
+        const factors: Factors = [[allNumbers[i], allNumbers[j]]];
+        palindromeFactors[candidate] = factors;
+      }
+    }
+  }
+
+  const smallestValue = Math.min.apply(Math, [...palindromeProducts]);
+  const largestValue = Math.max.apply(Math, [...palindromeProducts]);
+
+  return {
+    smallest: {
+      value: smallestValue,
+      factors: palindromeFactors[smallestValue],
+    },
+    largest: {
+      value: largestValue,
+      factors: palindromeFactors[largestValue],
+    },
+  };
 }
