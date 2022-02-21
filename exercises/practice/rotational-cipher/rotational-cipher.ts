2cf1363ecf96ac8854d87650058261bef88e93c5
Tim Hor <timothy.hor@outlook.com>
2022-02-21 17:54:56 +1100

    Start rotational-cipher

--- exercises/practice/rotational-cipher/rotational-cipher.ts
+++ exercises/practice/rotational-cipher/rotational-cipher.ts
@@ -1,3 +1,22 @@
-export function rotate() {
-  throw new Error('Remove this statement and implement this function')
+const plain = 'abcdefghijklmnopqrstuvwxyz';
+
+const getCipher = (offset: number) => {
+  const plainX2 = plain + plain;
+  return plainX2.slice(offset, offset + plain.length);
+};
+
+export function rotate(input: string, offset: number) {
+  const cipher = getCipher(offset);
+  const splitInput = [...input];
+  const rotatedInput = [];
+  for (const inputValue of splitInput) {
+    const isCapital = inputValue === inputValue.toLocaleUpperCase();
+    const inputValueIndex = plain.indexOf(inputValue.toLowerCase());
+    rotatedInput.push(
+      isCapital
+        ? cipher[inputValueIndex].toUpperCase()
+        : cipher[inputValueIndex],
+    );
+  }
+  return rotatedInput.join('');
 }
